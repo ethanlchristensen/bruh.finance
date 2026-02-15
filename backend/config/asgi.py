22acadb628c20f9b85f127aa4c2f94a88f073565
ethanlchristensen <ethanlchristensen@outlook.com>
2026-02-14 23:05:07 -0600

    [update] - erm

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,4 +1,8 @@
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -10,7 +14,7 @@ application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(  # Changed from AuthMiddlewareStack
+            JWTAuthMiddleware(
                 URLRouter(websocket_urlpatterns)
             )
         ),
